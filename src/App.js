9000a4a37b4af4f1852baf0be64a683ccd81fa35
Tammam Monther <tammamonther13@gmail.com>
2025-06-15 10:59:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Heading";
 import Button from "./components/Button";
 import { useState } from "react";
+
 const randNum = () => Math.floor(Math.random() * 100) + 1;
 function App() {
   const [num, setNum] = useState(0);
@@ -14,7 +15,8 @@ function App() {
       <Header firstname="bob" />,
       <Card h2="First card's h2" h3="First card's h3" num={randNum} />,
       <Card h2="Second card's h2" h3="Second card's h3" num={randNum} />,
-      <Button />
+      <Button />,
+      
     </>
   );
 }
